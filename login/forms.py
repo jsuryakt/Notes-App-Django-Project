6274b24718b870e8de2e70db8fd8935588762f49
jsuryakt <jsuryakt@gmail.com>
2020-06-15 20:00:18 +0530

    Django Notes Application

---
+++ login/forms.py
@@ -0,0 +1,20 @@
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from login.models import Notes
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class notesForm(forms.ModelForm):
+    class Meta:
+        model = Notes
+        fields = ['title','description']
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'input form-control col-5 shadow p-3 mb-5 bg-white rounded'}),
+            'description': forms.TextInput(attrs={'class':'input form-control form-control-lg col-8 shadow p-3 mb-5 bg-white rounded'}),
+        }
